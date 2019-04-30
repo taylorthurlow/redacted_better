92d5cfe8a29d89e8b8166aa8f5d07c5f0be00914
Taylor Thurlow <taylorthurlow8@gmail.com>
2019-04-29 19:32:50 -0700

    Add Group docs

--- lib/redacted_better/group.rb
+++ lib/redacted_better/group.rb
@@ -1,6 +1,11 @@
 class Group
-  attr_accessor :id, :name, :year, :record_label, :artists, :torrents
+  attr_accessor :id, :name, :year, :record_label, :artists
 
+  # @return [Array<Torrent>] the list of torrents within the group
+  attr_accessor :torrents
+
+  # @param data_hash [Hash] the data hash which comes directly from the Redacted
+  #   JSON API
   def initialize(data_hash)
     @id = data_hash["id"]
     @name = data_hash["name"]
@@ -10,6 +15,12 @@ class Group
     @torrents = []
   end
 
+  # Determines the artist name based on the number of contributing artists. Single artists
+  # are printed by themselves, two with a joining ampersand, and more than two with just
+  # "Various Artists".
+  #
+  # @return [String] a string representing the artist(s) responsible for the
+  #   group
   def artist
     case @artists.count
     when 1
@@ -21,6 +32,13 @@ class Group
     end
   end
 
+  # Sets the artist of a group to a single artist. Because the artists fields
+  # supports more than one artist, it is worth noting that this method does not
+  # support setting a value with more than one artist.
+  #
+  # @param new_artist [Hash] The artist information to add to the group,
+  #   containing an Integer `id` and a String `name`. The hash can be in either
+  #   string key or symbol key formats.
   def artist=(new_artist)
     @artists = [{
       "id" => new_artist[:id] || new_artist["id"],
@@ -28,6 +46,16 @@ class Group
     }]
   end
 
+  # Determines which formats are missing from a torrent by calculating which
+  # torrents are present within a group, and using a list of accepted formats
+  # to find which are missing.
+  #
+  # @param torrent [Torrent] The torrent to identify which release group we are
+  #   looking to query. A group may contain many release groups, and we need
+  #   one torrent from that group to find the specific release group.
+  #
+  # @return [Array<String, String>] A list of format/encoding pairs representing
+  #   all formats missing from the release group.
   def formats_missing(torrent)
     present = release_group_torrents(torrent).map { |t| [t.format, t.encoding] }
 
@@ -42,6 +70,11 @@ class Group
   # Given a torrent in a release group, find all torrents in the group which
   # are in the same release group. The returned list will also include the
   # torrent used to identify the release group.
+  #
+  # @param torrent [Torrent] the torrent used to identify the release group
+  #
+  # @return [Array<String, String>] a list of format/encoding pairs representing
+  #   all formats in the release group
   def release_group_torrents(torrent)
     torrents.select { |t| Torrent.in_same_release_group?(t, torrent) }
   end
