18160ef589758ee2721bd46e24d9b69fa8b1b61d
Taylor Thurlow <thurlow@hey.com>
2022-02-04 17:44:29 -0800

    Fix Utils not being module namespaced properly

--- lib/redacted_better/utils.rb
+++ lib/redacted_better/utils.rb
@@ -1,14 +1,16 @@
-class Utils
-  def self.deep_unescape_html(data)
-    case data
-    when Hash
-      data.map { |k, v| [k, deep_unescape_html(v)] }.to_h
-    when Array
-      data.map { |e| deep_unescape_html(e) }
-    when String
-      HTMLEntities.new.decode(data)
-    else
-      data
+module RedactedBetter
+  class Utils
+    def self.deep_unescape_html(data)
+      case data
+      when Hash
+        data.map { |k, v| [k, deep_unescape_html(v)] }.to_h
+      when Array
+        data.map { |e| deep_unescape_html(e) }
+      when String
+        HTMLEntities.new.decode(data)
+      else
+        data
+      end
     end
   end
 end
